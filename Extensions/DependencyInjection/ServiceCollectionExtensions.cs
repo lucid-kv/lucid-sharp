71a70377f6ce8436cb28d3400d1ef5089865545b
Clint.Network <me@clint.network>
2020-03-22 22:53:58 +0100

    Implement ServiceCollection extension

---
+++ Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lucid.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddLucid(this IServiceCollection services, Action<LucidOptions> options)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            services.Configure(options);
+
+            services.AddTransient<ILucid, Lucid>();
+            return services;
+        }
+    }
+}
