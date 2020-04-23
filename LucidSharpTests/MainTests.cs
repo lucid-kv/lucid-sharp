89da263019dc1e637d93ec89265c3b8d46ff38d6
Clint.Network <me@clint.network>
2020-04-23 23:57:28 +0200

    Create tests

---
+++ LucidSharpTests/MainTests.cs
@@ -0,0 +1,47 @@
+using System;
+using LucidSharp.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace LucidSharpTests
+{
+	public class Tests
+	{
+		private IDistributedCache _lucidCache;
+
+		[SetUp]
+		public void Setup()
+		{
+			var serviceProvider = new ServiceCollection()
+				.AddLucidCache(options =>
+				{
+					options.Configuration = "https://lucid-kv.herokuapp.com/";
+					options.InstanceName = "LucidPublicNode";
+				})
+				.BuildServiceProvider();
+			_lucidCache = serviceProvider.GetRequiredService<IDistributedCache>();
+		}
+
+		[Test, Order(1)]
+		public void Set()
+		{
+			_lucidCache.SetString("lucid-sharp", "Hello World!");
+			Assert.Pass();
+		}
+
+		[Test, Order(2)]
+		public void Get()
+		{
+			var value = _lucidCache.GetString("lucid-sharp");
+			Assert.AreEqual(value, "Hello World!");
+		}
+
+		[Test, Order(3)]
+		public void Delete()
+		{
+			_lucidCache.Remove("lucid-sharp");
+			Assert.Pass();
+		}
+	}
+}
